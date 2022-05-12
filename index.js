3ea2617a827d597984d3cdb151ad333ee06d4cca
gustavomgaspar <guga.more.gaspar@gmail.com>
2022-05-12 04:54:44 +0000

    0.2

--- index.js
+++ index.js
@@ -4,28 +4,45 @@ const Iserver = express();
 
 //configuração do DB
 const sequelize = require('sequelize');
-const db = new sequelize({dialect: 'sqlite', storage: 'DatabaseError.sqlite'});
+const db = new sequelize({dialect: 'sqlite', storage: 'Database.sqlite'});
 
 //model
 const Pessoa = db.define('pessoa',{
     id: {
-        type: sequelize.STRING,
+        type: sequelize.INTEGER,
+        autoIncrement: true,
         allownull: false,
         primaryKey: true
     },
     nome: {
         type: sequelize.STRING,
         allownull: false
+    },
+    email: {
+        type: sequelize.STRING,
+        allownull: false
     }
 });
 
 
-//rota principal
-Iserver.get('/', (req,res) => {
-    res.send("<h1>Funcionou</h1>");
+//newslatter
+Iserver.get('/newslatter/:nome/:email', (req,res) => {
+    (async () => {
+     
+        try {
+            const resultado = await db.sync();
+     
+            const resultadoCreate = await Pessoa.create({
+                nome: req.params.nome,
+                email: req.params.email
+            });
+            console.log(resultadoCreate);
+            res.send('<h1>Enviado</h1>');
+        } catch (error) {
+            console.log(error);
+        }
+    })();
 });
-
-
 //chamada do server
 Iserver.listen(3000, () => {
     console.log('Server iniciando na porta 3000');
